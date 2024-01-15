a77274cbdc8ed5457fa0b0ca589d6281dd677717
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-15 16:46:12 +0100

    Add files via upload

---
+++ ex03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    float a = 100;
+    float b = 101;
+    float c = 2; 
+    int moltiplicazione = a * b;
+    float divisione = moltiplicazione / c; 
+    int somma = divisione;
+
+    printf("moltiplicazione: %d \n", moltiplicazione);
+    printf("divisione: %f \n", divisione);
+    printf("somma: %d \n", somma);
+
+    return 0;
+}
