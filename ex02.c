2b9241c6629d95da5fcf587f7d564b54a627d743
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-15 15:21:18 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main () {
+    char mare = 'p';
+    char montagna = 'i'; 
+    char dajeroma = 'n'; 
+    char ole = 'o';
+    char ue = 'd';
+    char uo = 'a';
+    char ui = 'n';
+    char ao = 'i';
+    char oa = 'e';
+    char uu = 'l';
+    char oo = 'e';
+    printf("%c \n", mare);
+    printf("%c \n", montagna);
+    printf("%c \n", dajeroma);
+    printf("%c \n", ole);
+    printf("%c \n", ue);
+    printf("%c \n", uo);
+    printf("%c \n", ui);
+    printf("%c \n", ao);
+    printf("%c \n", oa);
+    printf("%c \n", uu);
+    printf("%c \n", oo);
+
+    return 0;
+}
