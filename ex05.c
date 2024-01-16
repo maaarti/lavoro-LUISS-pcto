9949aab36c525edd00b4100796cf983eb15c7749
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 09:47:10 +0100

    Add files via upload

---
+++ ex05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int neuroni;
+    printf("inserisci i neuroni di sofia e martina insieme\n");
+    scanf("%d", &neuroni);
+    printf("i neuroni di sofia e martina insieme sono: %d\n", neuroni);
+
+    return (0);
+}
