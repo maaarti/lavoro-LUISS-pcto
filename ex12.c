eef6567dd244173824f780cf826fd9fa81701d89
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 14:51:59 +0100

    Add files via upload

---
+++ ex12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    float c;
+    printf("inserisci una temperatura in Celsius: \n");
+    scanf("%f", &c);
+
+    if ( c <= -273.15 ){
+        printf("errore!! \n");
+    }
+    else{
+        printf("temperatura in Fahrenheit: %f \n", (1.8 * c) + 32);
+        printf("temperatura in Kelvin: %f \n", c + 273.15);
+    }
+    
+return (0);
+}
