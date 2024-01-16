61906ca25f1d03690fdbbb0a72e4c72ab51a1662
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 11:10:46 +0100

    Add files via upload

---
+++ ex08.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int n;
+    printf("inserisci il tuo voto in matematica: \n");
+    scanf("%d", &n);
+    if(n>6){
+        printf("sei un drago! \n");
+    }
+    else if(n<6){
+        printf("potevi impegnarti di più");
+    }
+    else if(n=6){
+        printf("ci sta ci sta");
+    }
+return (0);
+}
