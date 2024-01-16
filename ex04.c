0ac29070cb1433449cae4b5233884be5ac9df819
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 09:27:25 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    int numero;
+
+    printf("Inserisci un numero:\n");
+    scanf("%d", &numero);
+    printf("Il numero inserito è : %d\n", numero);
+    return (0);
+} 
