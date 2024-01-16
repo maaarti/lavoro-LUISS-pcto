c77b9ede56757c41c0f17efbb65e6e84748b75a8
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 14:52:15 +0100

    Add files via upload

---
+++ ex13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+    int c;
+    int d;
+    int e;
+    printf("inserisci un numero: \n");
+    scanf("%d", &c);
+    printf("inserisci un'altro numero: \n");
+    scanf("%d", &d);
+    printf("inserisci un'ultimo numero: \n");
+    scanf("%d", &e);
+    if (c - d == d - e); {
+        printf("i numeri sono in progressione aritmetica!! \n");
+    }if (c - d != d - e) {
+        printf("i numeri non sono in progressione aritmetica \n");
+    }
+    
+return (0);
+}
