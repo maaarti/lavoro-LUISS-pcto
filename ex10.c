bde7ae50590928460c0a4b4df3675214db423387
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 14:51:25 +0100

    Add files via upload

---
+++ ex10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    float c;
+    float d;
+    printf("inserisci a quanti anni si può prendere la patente nello stato in cui vivi: \n");
+    scanf("%f", &c);
+    printf("quanti anni hai? \n");
+    scanf("%f", &d);
+    if(d>=c){
+        printf("puoi prendere la patente!!! \n");
+    }
+    else if(d<=c){
+        printf("non puoi prendere la patente prova tra un po' di tempo! \n");
+    }
+return (0);
+}
