150bb19b8a1b2e058c055964d73e5eede40f4452
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 09:47:38 +0100

    Add files via upload

---
+++ ex06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    char iniziale;
+    printf("inserisci l'inziale del tuo cognome\n");
+    scanf("%c", &iniziale);
+    printf("l'iniziale del mio cognome è: %c\n", iniziale);
+
+    return (0);
+}
