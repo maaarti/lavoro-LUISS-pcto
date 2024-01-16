46cf84e31a4d57625c452d13fd09b528b4055653
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-16 16:52:52 +0100

    Add files via upload

---
+++ ex17.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int a = 4;
+    int b = 3;
+    int c = 3;
+
+    if(a == b && b == c){
+        printf("il triangolo è equilatero \n");
+    } else if ((a == b && b != c)||(a == c && b != c)||(c == b && b != a)) {
+        printf("il triangolo è isoscele \n");
+    } else {
+            printf("il triangolo è scaleno \n");
+        }
+
+    return (0);
+}
