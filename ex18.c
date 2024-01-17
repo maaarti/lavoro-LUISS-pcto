8ca19ce359fecdce2fbdab5d89347117819ba43e
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-17 09:37:04 +0100

    Add files via upload

---
+++ ex18.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+    int x;
+    int sottrazione1;
+    int sottrazione2;
+    printf("qual'è il tuo anno di nascita? \n");
+    scanf("%d", &x);
+    sottrazione1 = 1969 - x;
+    sottrazione2 = x - 1969;
+
+    if(x == 1969) {
+        printf("sei nato nell'anno in cui l'uomo è andato sulla Luna!! \n");
+    } else { 
+        if(x > 1969) {
+            printf("sei nato %d anni prima dell'anno in cui l'uomo è stato sulla Luna \n", sottrazione2);
+        } 
+        if(x < 1969) {
+            printf("sei %d anni dopo l'anno in cui l'uomo è stato sulla Luna \n", sottrazione1);
+        }
+    }
+
+    return (0);
+}
