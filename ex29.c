ccd983e87d16994f5be67e467629f9d0a83a1818
maaarti <156683354+maaarti@users.noreply.github.com>
2024-01-18 17:00:10 +0100

    Add files via upload

---
+++ ex29.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+    int main() {
+        char colore[17] = {'a','z','z','u','r','r','o','\0'};
+        printf("il colore preferito di marti è %s \n", colore);
+        return 0;
+    }
